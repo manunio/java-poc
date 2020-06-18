97772de6fc2c0f3046abbbba1e2d7792a56731e0
manunio <max.manu.nair@gmail.com>
2020-06-18 22:05:10 +0530

    update rce.sh

--- workspace/rce.sh
+++ workspace/rce.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # yes the arbitrary code does go here ;)
+mkdir -p hacked
 printf  '=============================\nHi LiveOverflow, code was executed :)\n=============================' | wall 
